3ff900fa4f82b78eb3d29fbdcc515061a553a945
Christopher Weems <hello@christopherweems.com>
2020-06-18 22:17:58 -0400

    Add `SiteSet` class

---
+++ Sources/SightIndex/SiteSet/SiteSet.swift
@@ -0,0 +1,49 @@
+//
+//  SiteSet.swift
+//  
+//
+//  Created by Christopher Weems on 6/18/20.
+//
+
+import Foundation
+import SightBuilder
+
+internal class SiteSet {
+    internal typealias Authority = String
+    fileprivate var sites: [Authority: Site]
+    
+    func site(forAuthority authority: Authority) -> Site? {
+        sites[authority]
+    }
+    
+    
+    // MARK: - Initializers
+        
+    fileprivate init(sites: [Authority: Site]) {
+        self.sites = sites
+    }
+    
+    convenience init(@SiteSetBuilder _ builder: () -> SiteSet) {
+        self.init(sites: builder().sites)
+    }
+    
+}
+
+
+// MARK: - SiteSetBuilder (Function Builder)
+
+@_functionBuilder
+internal struct SiteSetBuilder {
+    static func buildBlock(_ elements: Site...) -> SiteSet {
+        .init(sites: .init(sites: elements))
+    }
+}
+
+
+// MARK: - Helper Extensions
+
+fileprivate extension Dictionary where Key == SiteSet.Authority, Value == Site {
+    init(sites: [Site]) {
+        self.init(uniqueKeysWithValues: sites.map { ($0.authority, $0) })
+    }
+}
