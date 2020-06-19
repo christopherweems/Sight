b144d690ea4a3080d54df782c4e9154f0953ba2a
Christopher Weems <hello@christopherweems.com>
2020-06-18 22:23:56 -0400

    Implement Site.init?(authority:match:) initializer Remove ExpressibleByStringLiteral conformance [moving to Sight package]

--- Sources/SightIndex/Site+Extensions.swift
+++ Sources/SightIndex/Site+Extensions.swift
@@ -9,29 +9,27 @@ import Foundation
 import SightBuilder
 
 public extension Site {
-    init?(authority: String, match: AuthorityMatch = .exact) {
-        assert(!authority.contains("@"), "authority `userinfo` & `password` components not supported")
-        assert(!authority.contains(":"), "authority `port` component not supported")
-        assert(match == .exact, "best match not supported")
-        
-        fatalError("`Site.init(authority:_:)` is unimplemented")
-        
-    }
-}
-
-// MARK: - ExpressibleByStringLiteral conformance
-
-extension Site: ExpressibleByStringLiteral {
-    public init(stringLiteral value: String) {
-        self.init(authority: value, match: .exact)!
-    }
-}
-
-
-public extension Site {
+    private static var index = SiteIndex()
+    
     enum AuthorityMatch {
         case exact // O(1) for repeated matches
         case best  // non-deterministic & uncached
         
     }
+    
+    init?(authority: String, match: AuthorityMatch = .exact) {
+        assert(!authority.contains("@"), "authority `userinfo` & `password` components not supported")
+        assert(!authority.contains(":"), "authority `port` component not supported")
+        assert(match == .exact, "best match not supported")
+        
+        if let site = Self.index.site(forAuthority: authority) {
+            self = site
+        
+        } else {
+            return nil
+            
+        }
+        
+    }
+    
 }
