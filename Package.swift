f595bde318f01ccd2501cb548826966b4a0516ec
Christopher Weems <hello@christopherweems.com>
2020-06-19 16:03:43 -0400

    Add `unstandard` library dependency

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/christopherweems/unstandard.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,7 +31,7 @@ let package = Package(
             dependencies: ["SightBuilder", "SightIndex"]),
         .target(
             name: "SightBuilder",
-            dependencies: []),
+            dependencies: [.product(name: "unstandard", package: "unstandard")]),
         .target(
             name: "SightIndex",
             dependencies: ["SightBuilder"]),
