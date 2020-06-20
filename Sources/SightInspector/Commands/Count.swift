66f09d565bdd35bad5d67fe657f836d51e7c4fb4
Christopher Weems <hello@christopherweems.com>
2020-06-20 19:54:26 -0400

    Add `Count` command

---
+++ Sources/SightInspector/Commands/Count.swift
@@ -0,0 +1,18 @@
+//
+//  Count.swift
+//  
+//
+//  Created by Christopher Weems on 6/20/20.
+//
+
+import ArgumentParser
+import Foundation
+import SightBuilder
+import SightIndex
+
+struct Count: ParsableCommand {
+    
+    func run() throws {
+        print(SiteIndex().count)
+    }
+}
--- Sources/SightInspector/main.swift
+++ Sources/SightInspector/main.swift
@@ -14,7 +14,7 @@ struct SightInspector: ParsableCommand {
     static var configuration = CommandConfiguration(
             abstract: "A utility for generating search query urls & querying websites indexed by the Sight library.",
             version: "0.0.2",
-            subcommands: [PerformSearch.self],
+            subcommands: [Count.self, PerformSearch.self],
             defaultSubcommand: PerformSearch.self
         )
 }
