d2d63603efb5ff7f4d3f0bd108989151d14aa56f
Christopher Weems <hello@christopherweems.com>
2020-06-20 19:42:12 -0400

    Add SightInspector command

--- Sources/SightInspector/main.swift
+++ Sources/SightInspector/main.swift
@@ -10,4 +10,13 @@ import Foundation
 import SightBuilder
 import SightIndex
 
-PerformSearch.main()
+struct SightInspector: ParsableCommand {
+    static var configuration = CommandConfiguration(
+            abstract: "A utility for generating search query urls & querying websites indexed by the Sight library.",
+            version: "0.0.2",
+            subcommands: [PerformSearch.self],
+            defaultSubcommand: PerformSearch.self
+        )
+}
+
+SightInspector.main()
