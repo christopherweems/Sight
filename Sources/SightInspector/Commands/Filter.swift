6fa7e7f4a1cdf9063feda00deda64df763f2920c
Christopher Weems <hello@christopherweems.com>
2020-06-20 19:44:47 -0400

    Add `Filter` command

---
+++ Sources/SightInspector/Commands/Filter.swift
@@ -0,0 +1,15 @@
+//
+//  Filter.swift
+//  
+//
+//  Created by Christopher Weems on 6/20/20.
+//
+
+import ArgumentParser
+import Foundation
+import SightBuilder
+import SightIndex
+
+struct Filter: ParsableCommand {
+    
+}
