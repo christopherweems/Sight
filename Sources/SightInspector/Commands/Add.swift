0bb381c82e655e468373ea819e660a63ad161951
Christopher Weems <hello@christopherweems.com>
2020-06-30 11:12:05 -0400

    Add.run() now throws an error

--- Sources/SightInspector/Commands/Add.swift
+++ Sources/SightInspector/Commands/Add.swift
@@ -11,8 +11,14 @@ import SightBuilder
 import SightIndex
 
 struct Add: ParsableCommand {
+    enum Error: Swift.Error {
+        case insertionError
+    
+    }
     
     func run() throws {
-        
+        // In the future, this will insert the specified site url directly
+        // to the source files in /Sources/SightIndex/SitesByFirstCharacter/
+        throw Error.insertionError
     }
 }
