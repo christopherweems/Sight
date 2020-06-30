b610dc04c64f4344f97c6211647519b9a9d5036a
Christopher Weems <hello@christopherweems.com>
2020-06-30 10:16:06 -0400

    Add `Add` ParsableCommand

---
+++ Sources/SightInspector/Commands/Add.swift
@@ -0,0 +1,18 @@
+//
+//  Add.swift
+//  
+//
+//  Created by Christopher Weems on 6/30/20.
+//
+
+import ArgumentParser
+import Foundation
+import SightBuilder
+import SightIndex
+
+struct Add: ParsableCommand {
+    
+    func run() throws {
+        
+    }
+}
--- Sources/SightInspector/main.swift
+++ Sources/SightInspector/main.swift
@@ -13,8 +13,8 @@ import SightIndex
 struct SightInspector: ParsableCommand {
     static var configuration = CommandConfiguration(
             abstract: "A utility for generating search query urls & querying websites indexed by the Sight library.",
-            version: "0.0.4",
-            subcommands: [Count.self, PerformSearch.self, Validate.self],
+            version: "0.0.6",
+            subcommands: [Add.self, Count.self, PerformSearch.self, Validate.self],
             defaultSubcommand: PerformSearch.self
         )
 }
