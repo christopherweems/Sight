55df87087a1c459e32d8a047136386438b719e61
Christopher Weems <hello@christopherweems.com>
2020-07-06 12:14:04 -0400

    Update Format command to use ImportFile data structure

--- Sources/SightInspector/Commands/Format.swift
+++ Sources/SightInspector/Commands/Format.swift
@@ -10,13 +10,10 @@ import Foundation
 import SightBuilder
 import SightIndex
 
-#if canImport(AppKit)
-import AppKit
-#endif
-
 struct Format: ParsableCommand {
     enum Error: Swift.Error {
-        case missingImportFile
+        case importFileNotSpecified
+        case couldNotOpenImportFile
         
     }
     
@@ -27,67 +24,19 @@ struct Format: ParsableCommand {
     var replacementQuery: String?
     
     func validate() throws {
-        guard let _ = importPath else { throw Error.missingImportFile }
+        guard let _ = importPath else { throw Error.importFileNotSpecified }
         
     }
     
     func run() throws {
         let importFileURL = URL(fileURLWithPath: importPath!)
-        let searchURLs: [String]
-        let rootURLs: [String]
-        
-        do {
-            let lines = try String(contentsOf: importFileURL)
-                .split(separator: "\n", omittingEmptySubsequences: false)
-                .prefix { !$0.hasPrefix("#") }
-            
-            let sections = lines
-                .split(separator: "",maxSplits: 1)
-            
-            searchURLs = Array(sections[0])
-            rootURLs = (2 <= sections.count) ? Array(sections[1].filter { !$0.isEmpty }) : []
-            
-        }
-        catch {
-            searchURLs = []
-            rootURLs = []
-            
+        guard let importFile = ImportFile(contentsOf: importFileURL) else {
+            throw Error.couldNotOpenImportFile
         }
         
         // open non-prefixed urls
-        
-        rootURLs
-            .nonHTTPPrefixed
-            .compactMap { URL(string: "https://\($0)") }
-            .forEach(Self.open)
-        
-//        (searchURLs + rootURLs.httpPrefixed)
-//            .sightSorted()
-        
-    }
+        importFile.openSchemelessURLs()
     
-}
-
-
-fileprivate extension Format {
-    static func open(_ url: URL) {
-        #if canImport(AppKit)
-        NSWorkspace.shared.open(url)
-        #endif
     }
-}
-
-
-fileprivate extension Array where Element == String {
-    var httpPrefixed: Self {
-        self.filter { $0.hasPrefix("http") }
-    }
-    
-    var nonHTTPPrefixed: Self {
-        self.filter { !$0.hasPrefix("http") }
-    }
-    
-    func sightSorted() -> Self {
-        sorted()
-    }
+        
 }
