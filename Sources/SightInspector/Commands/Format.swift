7f369de229282bdd1e743bdca3a970c0be262057
Christopher Weems <hello@christopherweems.com>
2020-07-06 00:24:07 -0400

    Add `Format` ParsableCommand

---
+++ Sources/SightInspector/Commands/Format.swift
@@ -0,0 +1,93 @@
+//
+//  Format.swift
+//  
+//
+//  Created by Christopher Weems on 7/5/20.
+//
+
+import ArgumentParser
+import Foundation
+import SightBuilder
+import SightIndex
+
+#if canImport(AppKit)
+import AppKit
+#endif
+
+struct Format: ParsableCommand {
+    enum Error: Swift.Error {
+        case missingImportFile
+        
+    }
+    
+    @Option(name: .shortAndLong, help: "Path of import file")
+    var `importPath`: String?
+    
+    @Option(name: .shortAndLong, help: "The query used to discover the search page")
+    var replacementQuery: String?
+    
+    func validate() throws {
+        guard let _ = importPath else { throw Error.missingImportFile }
+        
+    }
+    
+    func run() throws {
+        let importFileURL = URL(fileURLWithPath: importPath!)
+        let searchURLs: [String]
+        let rootURLs: [String]
+        
+        do {
+            let lines = try String(contentsOf: importFileURL)
+                .split(separator: "\n", omittingEmptySubsequences: false)
+                .prefix { !$0.hasPrefix("#") }
+            
+            let sections = lines
+                .split(separator: "",maxSplits: 1)
+            
+            searchURLs = Array(sections[0])
+            rootURLs = (2 <= sections.count) ? Array(sections[1].filter { !$0.isEmpty }) : []
+            
+        }
+        catch {
+            searchURLs = []
+            rootURLs = []
+            
+        }
+        
+        // open non-prefixed urls
+        
+        rootURLs
+            .nonHTTPPrefixed
+            .compactMap { URL(string: "https://\($0)") }
+            .forEach(Self.open)
+        
+//        (searchURLs + rootURLs.httpPrefixed)
+//            .sightSorted()
+        
+    }
+    
+}
+
+
+fileprivate extension Format {
+    static func open(_ url: URL) {
+        #if canImport(AppKit)
+        NSWorkspace.shared.open(url)
+        #endif
+    }
+}
+
+
+fileprivate extension Array where Element == String {
+    var httpPrefixed: Self {
+        self.filter { $0.hasPrefix("http") }
+    }
+    
+    var nonHTTPPrefixed: Self {
+        self.filter { !$0.hasPrefix("http") }
+    }
+    
+    func sightSorted() -> Self {
+        sorted()
+    }
+}
--- Sources/SightInspector/main.swift
+++ Sources/SightInspector/main.swift
@@ -14,7 +14,7 @@ struct SightInspector: ParsableCommand {
     static var configuration = CommandConfiguration(
             abstract: "A utility for generating search query urls & querying websites indexed by the Sight library.",
             version: "0.0.7",
-            subcommands: [Add.self, Count.self, Describe.self, Validate.self],
+            subcommands: [Add.self, Count.self, Describe.self, Format.self, Validate.self],
             defaultSubcommand: Describe.self
         )
 }
