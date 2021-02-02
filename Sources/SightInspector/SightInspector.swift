7f56f802c46aa79fe2160679a91f3807a460f8fa
Christopher Weems <hello@christopherweems>
2021-02-02 15:49:03 -0500

    Replace `main.swift` with `SightInspector.swift`

--- Sources/SightInspector/SightInspector.swift
+++ Sources/SightInspector/SightInspector.swift
@@ -1,8 +1,8 @@
 //
-//  main.swift
+//  SightInspector.swift
 //  
 //
-//  Created by Christopher Weems on 6/19/20.
+//  Created by Christopher Weems on 2/2/21.
 //
 
 import ArgumentParser
@@ -10,13 +10,13 @@ import Foundation
 import SightBuilder
 import SightIndex
 
+@main
 struct SightInspector: ParsableCommand {
     static var configuration = CommandConfiguration(
             abstract: "A utility for generating search query urls & querying websites indexed by the Sight library.",
-            version: "0.0.7",
+            version: "0.1.0",
             subcommands: [Add.self, Count.self, Describe.self, Format.self, Validate.self],
             defaultSubcommand: Describe.self
         )
+    
 }
-
-SightInspector.main()
