a453aa77e3ee33559496697b129b63ec24423365
Christopher Weems <hello@christopherweems>
2021-02-02 15:45:09 -0500

    Update minimum `Resultto` & `unstandard` versions

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "0.3.2")),
-        .package(url: "https://github.com/christopherweems/unstandard.git", .upToNextMajor(from: "0.0.21")),
+        .package(url: "https://github.com/christopherweems/unstandard.git", .upToNextMajor(from: "0.1.0")),
         .package(url: "https://github.com/gitmcfly/lc-locale.git", .upToNextMajor(from: "0.0.1")),
         
     ],
